6c083d61404605d4fc2b04f45a24ce7a3f8d648e
Henry Tran <hhtran1122@gmail.com>
2016-01-31 15:05:21 -0800

    Finally fix interface for card and player

--- app/card.ts
+++ app/card.ts
@@ -1,12 +1,8 @@
-export class card {
-  id : number;
-  public num : string;
-  public suit : string;
-  public title : string;
-  constructor (public id, public num, public suit, public title) {
-    this.id = id;
-    this.num = num;
-    this.suit = suit;
-    this.title = this.num & this.suit;
-  }
+import {Player} from "./player"
+
+export interface Card {
+  id: number;
+  player: Player;
+  num: string;
+  suit: string;
 }
--- app/sg13.ts
+++ app/sg13.ts
@@ -1,25 +1,46 @@
 import {Component} from 'angular2/core';
-import {Card} from './card.ts';
+import {Card} from './card';
+import {Player} from './player';
 
 @Component({
   selector: 'sg13',
   template: `
-    <h2>Todo</h2>
-    <span> of {{cards.length}} remaining</span>
+    <h2>SG13 <input type="number" value="{{_maxPlayer}}"></h2>
+    <span> of {{Cards.length}} remaining</span>
     <ul>
-      <li *ngFor="#card of cards">
-        <span>{{card.num}} and {{card.suit}} total {{card.title}}</span>
+      <li *ngFor="#card of Cards">
+        <span>{{card.num}} and {{card.suit}} playered to {{card.player}}</span>
       </li>
     </ul>
-    [ <a (click)="archive()">archive</a> ]
+    [ <a (click)="newGame()">New Game</a> ]
     `
 })
 export class sg13 {
-  cards: Card[] = [
-      {id:1, num:"3", suit: "C"},
-      {id:2, num:"3", suit: "D" }
+  Players: Player[] = [
+    { id: 1},
+    { id: 2},
+    { id: 3},
+    { id: 4}
   ];
-  get remaining() {
-    return this.cards.reduce((count: number, card: Card) =>);
+  _maxPlayer: number = 4;
+  Cards: Card[] = [
+    { id: 1, num: "3", suit: "C", player: { id: 1 } },
+    { id: 2, num: "3", suit: "D", player: { id: 1 } },
+    { id: 3, num: "3", suit: "S", player: { id: 1 } },
+    { id: 4, num: "3", suit: "H", player: { id: 1 } },
+    { id: 5, num: "4", suit: "C", player: { id: 1 } },
+    { id: 6, num: "4", suit: "D", player: { id: 1 } },
+    { id: 7, num: "4", suit: "S", player: { id: 1 } },
+    { id: 8, num: "4", suit: "H", player: { id: 1 } },
+    { id: 9, num: "5", suit: "C", player: { id: 1 } },
+    { id: 10, num: "5", suit: "D", player: { id: 1 } },
+    { id: 11, num: "5", suit: "S", player: { id: 1 } },
+    { id: 12, num: "5", suit: "H", player: { id: 1 } },
+  ];
+  newGame() {
+    this.Cards.forEach(card  => {
+      var t_player = Math.floor((Math.random() * this._maxPlayer) + 1);
+      card.player = {id: t_player };
+    });
   }
 }
